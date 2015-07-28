46d4af73af87aad892d9f5bf1271b7147645f091
KateMShirley <kate.m.shirley@gmail.com>
2015-07-28 16:29:24 -0500

    starting sinatra_coupons app with Isaac

---
+++ sinatra_coup_migration.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE sinatra_coupons
+
+\c sinatra_coupons
+
+CREATE TABLE sinatra_coupons (id SERIAL PRIMARY KEY, title varchar (255), description text, product int, discount money, exp_date date);
+
+INSERT INTO sinatra_coupons (title, description, product, discount, exp_date)
+VALUES
+('BOGO', 'Buy one get one free', 123456, 10.00, '2015, 12, 31');
+
+INSERT INTO sinatra_coupons (title, description, product, discount, exp_date)
+VALUES
+('Free', 'Get your stuff for free!', 1234567, 0.00, '2020, 12, 31');
